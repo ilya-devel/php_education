51260865536038bc1216c62b47545f77b9ceb111
ilya.devel <ilya.devel@yandex.ru>
2024-08-21 22:20:25 +0500

    sem1 homework completed

--- php-fpm/code/index.php
+++ php-fpm/code/index.php
@@ -1,7 +1,21 @@
 <?php
+$a = 5;
+$b = '05';
+var_dump($a == $b); // bool(true) не строгое сравнение с неявным приведением второй переменной к целому числу
+var_dump((int) '012345'); // int(12345) явное приведение строки к целому числу
+var_dump((float) 123.0 === (int) 123.0); // bool(false) строгое сравнение 2 переменных разного типа
+var_dump(0 == 'hello, world');
+    // 8.2 bool(false) нет возможности привести строку к целому числу, а т.к. значения разные то false
+    // 7.4 bool(true) т.к. не может преобразовать строку к целому, преобразует её в 0, поэтому true
 
-echo "Hello everybody<br>".date("Y-m-d H:i:s") ."<br><br>";
 
-echo "Something";
 
-phpinfo();
+$first = 10;
+$second = 2;
+echo "first = $first and second = $second<br>";
+
+$first+=+$second-$second=$first;
+echo "first = $first and second = $second<br>";
+
+$first = $second + $first - ($second = $first);
+echo "first = $first and second = $second<br>";
--- sem1_homeworks/task1.php
+++ sem1_homeworks/task1.php
@@ -4,13 +4,18 @@ $b = '05';
 var_dump($a == $b); // bool(true) не строгое сравнение с неявным приведением второй переменной к целому числу
 var_dump((int) '012345'); // int(12345) явное приведение строки к целому числу
 var_dump((float) 123.0 === (int) 123.0); // bool(false) строгое сравнение 2 переменных разного типа
-var_dump(0 == 'hello, world'); // bool(false) нет возможности привести строку к целому числу, а т.к. значения разные то ложь
+var_dump(0 == 'hello, world');
+    // 8.2 bool(false) нет возможности привести строку к целому числу, а т.к. значения разные то false
+    // 7.4 bool(true) т.к. не может преобразовать строку к целому, преобразует её в 0, поэтому true
 
 
 
 $first = 10;
 $second = 2;
-echo "first = $first and second = $second\n";
+echo "first = $first and second = $second<br>";
 
 $first+=+$second-$second=$first;
-echo "first = $first and second = $second\n";
+echo "first = $first and second = $second<br>";
+
+$first = $second + $first - ($second = $first);
+echo "first = $first and second = $second<br>";
